fbd3d5e70b37458d164e0702d32b2bfa0040efda
Lehat Khalid <lehatk@hotmail.se>
2023-12-07 15:00:56 +0100

    initial code commit

--- taxi-app/src/App.js
+++ taxi-app/src/App.js
@@ -3,21 +3,18 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="view">
+      <div className="header">TAXI DIREKT</div>
+      <div className="login">
+        <label htmlFor="username">USERNAME</label>
+        <textarea id="username" placeholder="Default Username"></textarea>
+      </div>
+
+      <div className="login">
+        <label htmlFor="password">PASSWORD</label>
+         <input type="password" id="password" placeholder="Enter you password"/>
+      </div>
+      <button onClick={() => alert('You are logging in')}>LOGIN</button>
     </div>
   );
 }
